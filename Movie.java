125cd20adf7b60fcf1c41e7c09737554297d57cb
Nawakanok Muangkham <guitar@Guitar-2.local>
2019-05-06 18:29:32 +0700

    add movieID

--- Movie.java
+++ Movie.java
@@ -2,17 +2,21 @@ import java.util.ArrayList;
 
 public class Movie
 {
+    private int movieID;
     private String name = null;
     private ArrayList<String> genre ;
     private int releaseYear;
     private double rating = 0;
     private ReviewCollection reviewList = new ReviewCollection();
+    private static int count = 0;
 
     public Movie(String name, ArrayList<String> type, int year)
     {
         this.name = name;
         this.genre = type;
         this.releaseYear = year;
+        count++;
+        this.movieID=count;
     }
 
     public String getName()
